dc259dfee04f0aef5f0fad230acb10094ba35407
Andria <chang.andria@gmail.com>
2023-08-22 23:10:24 +0200

    corrected misspelling

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import { InfinitySpin } from 'react-loader-spinner'
 ;
 export default function Weather(props){
   function handleResponse(response){
-    alert(`The weather in ${response.data.name} is ${tresponse.data.main.temp}C`
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}C`
   );
 }
 let apiKey= "0d206f601e5e6e86a658ffd9322e991a";
