c88c832e0aff31db4033271c4cbc49eba019fcdb
Andria <chang.andria@gmail.com>
2023-08-23 23:33:59 +0200

    added bootstrap
    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Weather from './Weather';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by {" "}
-        <a href="https://www.linkedin.com/in/andria-c/" target="_blank" rel="noreferrer"> Andria
-        </a> and is {" "}
-        <a href="https://github.com/andria-coder/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
-      </footer>
-
+      <div className="container m-3">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {" "}
+          <a href="https://www.linkedin.com/in/andria-c/" target="_blank" rel="noreferrer"> Andria
+          </a> and is {" "}
+          <a href="https://github.com/andria-coder/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
+        </footer>
+      </div>
     </div>
   );
 }
